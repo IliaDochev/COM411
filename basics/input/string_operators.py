ad3134193f632217c22e5e87d322fe4fe456bcba
repl.it user <replituser@example.com>
2021-03-05 20:11:15 +0000

    Added code to demonstrate the use of string operators.

---
+++ basics/input/string_operators.py
@@ -0,0 +1,7 @@
+lives = int(input("Please enter the number of lives.\n"))
+energy = int(input("\nPlease enter the energy level.\n"))
+shield = int(input("\nPlease enter the shield level.\n"))
+
+print("Lives:", "♥" * lives)
+print("Energy:", "➾" * energy)
+print("Shield:", "♦" * shield)
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print ("Hello33")
+choice = int(input("Please select a program to run:\n1 - Data Types\n"))
+
+
+if (choice<=1):
+ run = "python basics/input/data_types.py"
