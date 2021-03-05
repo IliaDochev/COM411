345283be7be237cec58205571b4a7bf644cc62f9
repl.it user <replituser@example.com>
2021-03-05 21:29:13 +0000

    Added comments to previous programs

--- basics/decisions/simple_decision/if.py
+++ basics/decisions/simple_decision/if.py
@@ -1,6 +1,8 @@
+#Requesting the kind of book from the user and storing it in the variable "book"
 book = str(input("What type of book is this?\n"))
 
-if (book == "adventure"):
+# Performing the check to see if the user's choice matches the liking of Beep
+if (book == "adventure"): # If the check comes back as a match we print the below statements
   print("\n\nI like adventure books\n")
   print("Finished reading the book!")
 
--- basics/decisions/simple_decision/if_else.py
+++ basics/decisions/simple_decision/if_else.py
@@ -1,11 +1,12 @@
+# The below line requests from the user to enter an activity and stores it
 print("Please enter the activity you wish Beep to perform:")
 activity = input() 
 
 
-if (activity == "calculate"):
+if (activity == "calculate"): #This line checks to see if the user choice is to calculate and if it matches, it runs the below code
     print("\n\nPerforming calculations...")
-else:
+else: #If the above check comes out as false it prints the below line of code
     print("\nPerforming activity...")
 
-
+#Informing the user that whichever activity he chose is complete
 print("\n\n\nActivity completed!")
