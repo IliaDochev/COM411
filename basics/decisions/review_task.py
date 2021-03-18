05acd02f310bc193d82eee83b2e7ac462c998d42
iliyadochev <5dochi30@solent.ac.uk>
2021-03-18 19:12:22 +0000

    test

---
+++ basics/decisions/review_task.py
@@ -0,0 +1,10 @@
+print("Please enter the value of your shopping cart.")
+cart = input(float())
+
+
+print("Please enter the distance from the store you live at.")
+distance = input(float())
+
+
+if(cart >= 100) and (distance <= 10):
+    print("thank you")
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Program sta")
+frog = [3, 50, -14, 11]
+superfrog = [3, "banana", 150, -60, "apple", "elephant"] #Lists
+
+print("Content of superfrog:")
+
+for pig in superfrog:
+    print(pig, end="\t")
+
+print("\nContent of frog:")
+
+for i in frog:
+    print(i)
+
+print("Bye")
